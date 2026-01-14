4d971c6ce3939a1c320fa2fa10f836524c1a0a2f
404egor <ekrohmalnij@gmail.com>
2026-01-14 16:44:02 +0200

    second commit

--- task2_1.py
+++ task2_1.py
@@ -4,11 +4,13 @@ def total_salary(path):
     with open(path, 'r', encoding='utf-8') as file:
         for line in file:
             employee, salary = line.split(",")
-            salaries.append(int(salary))
+            salaries.append(float(salary))
+    try:
+        total = sum(salaries)
+        average_salary = total/len(salaries)
+    except ZeroDivisionError:
+        print("Зарплат немає")
 
-    total = sum(salaries)
-    average_salary = total//len(salaries)
-    
     return(total, average_salary)
     
 total, average = total_salary(r"Text.txt")
--- task2_2.py
+++ task2_2.py
@@ -7,7 +7,7 @@ def get_cats_info(path):
         with open(path, 'r', encoding='utf-8') as file:
             for line in file:
                 id, name, age = line.strip().split(',')
-                cats_dict.append({'id': id, 'name': name, 'age': int(age)})
+                cats_dict.append({'id': id, 'name': name, 'age': age})
     except FileNotFoundError:
         print("Файл не знайдено")
     except ValueError:
